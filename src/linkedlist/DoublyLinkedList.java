16db3a33e43296a8a94ade029c10a9694e7db139
Ajith Kumar <aku.ajith.kumar.ms@gmail.com>
2025-03-18 10:55:25 +0530

    Added DoublyLinkedList

---
+++ src/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,56 @@
+package linkedlist;
+
+public class DoublyLinkedList {
+
+    static class Node {
+        private final String data;
+        private Node next;
+        private Node prev;
+
+        public Node(String data) {
+            this.data = data;
+        }
+    }
+
+    Node head = null;
+
+    public void addNode(String data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Nodes of doubly linked list: ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dList = new DoublyLinkedList();
+        // Add nodes to the list
+        dList.addNode("1");
+        dList.addNode("2");
+        dList.addNode("3");
+        dList.addNode("4");
+        dList.addNode("5");
+
+        dList.display();
+    }
+}
