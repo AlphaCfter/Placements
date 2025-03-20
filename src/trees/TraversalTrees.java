07b3eb93e9f6090e2b427a12bdea7461b202afb0
Ajith Kumar <aku.ajith.kumar.ms@gmail.com>
2025-03-20 16:12:36 +0530

    Added Tree traversal

--- src/trees/TraversalTrees.java
+++ src/trees/TraversalTrees.java
@@ -1,6 +1,6 @@
 package trees;
 
-public class Trees {
+public class TraversalTrees {
 
     static class Node {
         int value;
@@ -17,7 +17,7 @@ public class Trees {
     // Root of the tree
     public Node root;
 
-    Trees() {
+    TraversalTrees() {
         this.root = null;
     }
 
@@ -29,33 +29,26 @@ public class Trees {
         }
     }
 
+
     private void addRecursive(Node node, int value) {
         if (node.left == null) {
             node.left = new Node(value);
             return;
         }
 
+
         if (node.right == null) {
             node.right = new Node(value);
             return;
         }
 
-
         addRecursive(node.left, value);
     }
-
 
     public void printInOrder() {
         printInOrderRecursive(root);
     }
 
-    public void printPreOrder() {
-        printPreOrderRecursive(root);
-    }
-
-    public void printPostOrder() {
-        printPostOrderRecursive(root);
-    }
     private void printInOrderRecursive(Node node) {
         if (node != null) {
             printInOrderRecursive(node.left);
@@ -64,26 +57,9 @@ public class Trees {
         }
     }
 
-    private void printPreOrderRecursive(Node node) {
-        if (node != null) {
-            System.out.print(node.value + " ");
-            printInOrderRecursive(node.left);
-            printInOrderRecursive(node.right);
-        }
-    }
-
-    private void printPostOrderRecursive(Node node) {
-        if (node != null) {
-            printInOrderRecursive(node.left);
-            printInOrderRecursive(node.right);
-            System.out.print(node.value + " ");
-        }
-    }
-
     public static void main(String[] args) {
-        Trees tree = new Trees();
+        TraversalTrees tree = new TraversalTrees();
 
-        // Add some values to the tree
         tree.add(10);
         tree.add(5);
         tree.add(15);
@@ -94,10 +70,5 @@ public class Trees {
 
         System.out.println("In-order traversal of the Binary Tree:");
         tree.printInOrder();
-        System.out.println("Pre-order traversal of the Binary Tree:");
-        tree.printPreOrder();
-        System.out.println("Post-order traversal of the Binary Tree:");
-        tree.printPostOrder();
-
     }
 }
